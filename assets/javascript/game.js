5deb86cb63c0c4ea5f363de9f66116178d178c2a
Esther Hwang <estshwang@gmail.com>
2019-04-29 16:56:20 -0500

    will is fading from my soul, still trying to figure out how to assign a different random number to each crystal. Took out some pseudo coding to see format more clearly. added crystal images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,50 @@
 $(function() {
 
-//Random number generator
-let guessNumber = Math.floor(Math.random() * 101) + 19;
-console.log("random number is ", guessNumber);
-
-//puts random number into div
-$('#compNumber').text(guessNumber);
-
 //player starting score
 let playerScore = 0;
-
+let guessNumber = 0;
 var wins = 0;
 var loss = 0;
 
 
-class crystalBox {
-    constructor() {
 
 
-  }
-}
 
+//to start game
 function startGame() {
-    //reset cyrstal value assigments
+   
     //reset random number generator
-
-
-
-}
-//for loop that gives each crystal a data value set to numberOptions
-for (let i=0; i<4; i++) {
-
-    //variable with random value assignment for each crystal
+    let guessNumber = Math.floor(Math.random() * 101) + 19;
+    console.log("random number is ", guessNumber);
+    
+    //puts random number into div
+    $("#compNumber").text(guessNumber);
+    
     let numberOptions = Math.floor(Math.random() * 12) + 1;
     console.log("generating: ", numberOptions);
-
-    let crystals = $('.crystalBtn')
-    //setting 'data-crystal-value' attribute to each tag that has .crystalBtn class
-    crystals.attr('data-crystal-value', numberOptions);
-
-    //append each item to the affected img tags
-    $('.crystalBtn').append(crystals);
     
+    //reset cyrstal value assigments 
+    for (let i=0; i<4; i++) {
     
-}
+        //variable with random value assignment for each crystal
+       
+    
+        //setting 'data-crystal-value' attribute to each tag that has .crystalBtn class
+        $("img").val(numberOptions);
+    
+        //how to assign four different values to the different crystals.
 
-$('.crystalBtn').on('click', function() {
-        
-    let crystalValue = $(this).attr(numberOptions);
-        
-        //converting string value into number 
-        crystalValue = parseInt(crystalValue);
+    }
+  }
 
-        //adds crystal value to player score
-        playerScore += crystalValue; 
+ 
+//event listener for crystal
+$(".crystalBtn").on("click", function() {
+    
+    console.log($(".crystalBtn").attr("data-crystal-value"));
+  
 
-        //display new total score 
-        $('#yourScore').html(playerScore);
-
-        //if player wins
-        if(playerScore === guessNumber) {
-          
-            $('#winLoseAnnounce').html('<h2>You Won!</h2>');
-
-            wins++;
-        
-        } else if (playerScore >= guessNumber) {
-            
-            $('#winLoseAnnounce').text('<h2>You Lost!</h2>');
-
-            loss++;
-
-            startGame();
-        }
-    });
+  });
 
 startGame();
 
